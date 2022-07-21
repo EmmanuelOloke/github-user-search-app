e866ac6e79ca9d1239d277553c982aef0a2fb9b7
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-21 02:56:04 +0100

    octocat image import and some styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Box, Text, IconButton, Input, Button } from '@chakra-ui/react';
+import { Box, Text, IconButton, Input, Button, Image } from '@chakra-ui/react';
 
 import { FaMoon, FaSearch } from 'react-icons/fa';
+
+import octocat from './image/octocat.png';
 
 function App() {
   return (
@@ -12,13 +14,13 @@ function App() {
       justifyContent="center"
       alignItems="center"
     >
-      <Box w={800} border="1px" borderColor="red.200">
+      <Box w={700} border="1px" borderColor="red.200">
         <Box
           display="flex"
           alignItems="center"
           flexDirection="row"
           justifyContent="space-between"
-          my={8}
+          mb={4}
         >
           <Text fontWeight="bold" fontSize="2xl">
             devfinder
@@ -26,16 +28,46 @@ function App() {
           <IconButton icon={<FaMoon />} isRound="true" />
         </Box>
 
-        <Box display="flex">
-          <FaSearch />
-          <Input
-            pos="relative"
-            placeholder="Search GitHub username..."
-            py="6"
-          />
-          <Button pos="absolute" right colorScheme="blue" px="8" type="submit">
+        <Box
+          borderColor="red"
+          border="1px"
+          display="flex"
+          alignItems="center"
+          gap={3}
+          backgroundColor="#ffffff"
+          p={3}
+          borderRadius="lg"
+        >
+          <FaSearch color="blue" fontSize="30px" fontWeight="normal" />
+          <Input placeholder="Search GitHub username..." py="" />
+          <Button colorScheme="blue" color="white" px="7" py="6" type="submit">
             Search
           </Button>
+        </Box>
+
+        <Box display="flex" flexDirection="column" px="6" py="6">
+          <Box display="flex" flexDirection="row" alignItems="flex-start">
+            <Image src={octocat} w="7rem" borderRadius="50%" mr={10} />
+
+            <Box display="flex" flexDirection={{}}>
+              <Box
+                mr={{
+                  sm: '25',
+                  md: '28',
+                  lg: '30',
+                  xl: '28',
+                  base: '25',
+                }}
+              >
+                <Text fontWeight="bold" fontSize="2xl">
+                  The Octocat
+                </Text>
+                <Text color="blue">@octocat</Text>
+              </Box>
+
+              <Box mt={{ lg: '1' }}>Joined 25 Jan 2011</Box>
+            </Box>
+          </Box>
         </Box>
       </Box>
     </Box>
