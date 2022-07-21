3cc252078babcab8bc77a3b2e9cb1b609949cb24
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-21 04:12:53 +0100

    repo, followers and following section

--- src/App.js
+++ src/App.js
@@ -50,27 +50,68 @@ function App() {
           </Button>
         </Box>
 
-        <Box display="flex" flexDirection="column" px="6" py="6">
-          <Box display="flex" flexDirection="row" alignItems="flex-start">
+        <Box display="flex" flexDirection="column" px="10" py="6">
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="flex-start"
+            justifyContent="space-between"
+          >
             <Image src={octocat} w="7rem" borderRadius="50%" mr={10} />
 
-            <Box display="flex" flexDirection={{}}>
-              <Box
-                mr={{
-                  sm: '25',
-                  md: '28',
-                  lg: '30',
-                  xl: '28',
-                  base: '25',
-                }}
-              >
-                <Text fontWeight="bold" fontSize="2xl">
-                  The Octocat
-                </Text>
-                <Text color="blue">@octocat</Text>
+            <Box display="flex" flexDirection="column">
+              <Box display="flex" flexDirection={{ lg: 'row' }} mb={5}>
+                <Box
+                  mr={{
+                    sm: '25',
+                    md: '28',
+                    lg: '35',
+                    xl: '28',
+                    base: '25',
+                  }}
+                >
+                  <Text fontWeight="bold" fontSize="2xl">
+                    The Octocat
+                  </Text>
+                  <Text color="blue">@octocat</Text>
+                </Box>
+
+                <Box mt={{ lg: '1' }}>Joined 25 Jan 2011</Box>
               </Box>
 
-              <Box mt={{ lg: '1' }}>Joined 25 Jan 2011</Box>
+              <Text>This profile has no bio.</Text>
+
+              <Box
+                display="flex"
+                flexDirection="row"
+                justifyContent="space-between"
+                backgroundColor="#f4f7ff"
+                borderRadius="lg"
+                px={10}
+                py={5}
+                mt={10}
+              >
+                <Box display="flex" flexDirection="column">
+                  <Text fontSize="sm">Repo</Text>
+                  <Text fontSize="2xl" fontWeight="bold">
+                    8
+                  </Text>
+                </Box>
+
+                <Box display="flex" flexDirection="column">
+                  <Text fontSize="sm">Followers</Text>
+                  <Text fontSize="2xl" fontWeight="bold">
+                    6433
+                  </Text>
+                </Box>
+
+                <Box display="flex" flexDirection="column">
+                  <Text fontSize="sm">Following</Text>
+                  <Text fontSize="2xl" fontWeight="bold">
+                    9
+                  </Text>
+                </Box>
+              </Box>
             </Box>
           </Box>
         </Box>
