91b53474b39f14f028444fa24beae28f81717af0
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-21 01:09:09 +0100

    import font weights and add theme as props on ChakraProvider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
+import '@fontsource/space-mono/400.css';
+import '@fontsource/space-mono/700.css';
+
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode="light" />
       <App />
     </ChakraProvider>
